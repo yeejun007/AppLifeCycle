72cfe7a71e04ec49261c88bfb78981a7fd0a81d7
yeejun007 <dlwns007@gmail.com>
2020-07-24 15:37:06 +0900

    Initial Commit

---
+++ AppLifeCycle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppLifeCycle
+//
+//  Created by 송이준 on 24/07/2020.
+//  Copyright © 2020 yeejun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AppLifeCycle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppLifeCycle
+//
+//  Created by 송이준 on 24/07/2020.
+//  Copyright © 2020 yeejun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
